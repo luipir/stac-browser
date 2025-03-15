4ebd15a1ffef74fc5b095ecc515e1797df6feda1
StefanoMorroni <smorroni@hotmail.com>
2025-03-15 15:57:19 +0100

    revert

--- config.js
+++ config.js
@@ -1,81 +1,51 @@
 module.exports = {
-    catalogUrl: "https://geoportale-preprod.comune.roma.it/stacapi/",
+    catalogUrl: null,
     catalogTitle: "STAC Browser",
-    allowExternalAccess: false, // Must be true if catalogUrl is not given
+    allowExternalAccess: true, // Must be true if catalogUrl is not given
     allowedDomains: [],
     detectLocaleFromBrowser: true,
     storeLocale: true,
-    locale: "it",
-    fallbackLocale: "it",
+    locale: "en",
+    fallbackLocale: "en",
     supportedLocales: [
-<<<<<<< HEAD
         "de",
         "ar",
-=======
-        // "de",
->>>>>>> 2a93bd82 (logo & colors)
 //      "de-CH",
-        // "es",
-        // "en",
+        "es",
+        "en",
 //      "en-GB",
 //      "en-US",
-        // "fr",
+        "fr",
 //      "fr-CA",
 //      "fr-CH",
         "it",
 //      "it-CH",
-        // "ro",
-        // "ja",
-        // "pt",
+        "ro",
+        "ja",
+        "pt",
 //      "pt-BR"
     ],
     apiCatalogPriority: null,
     useTileLayerAsFallback: false,
     displayGeoTiffByDefault: false,
-    // buildTileUrlTemplate: null,
-    // buildTileUrlTemplate: asset => "https://tiles.rdnt.io/tiles/{z}/{x}/{y}@2x?url=" + encodeURIComponent(asset.getAbsoluteUrl()),
-    buildTileUrlTemplate: async (
-        asset
-    ) => {
-        let modifiedHref = asset.href.replace("/aerofototeca/integration/api/download", "/aerofototeca-api/integration/api/tile");
-        return "https://geoportale-preprod.comune.roma.it/titiler/cog/tiles/WebMercatorQuad/{z}/{x}/{y}@2x?url=" + encodeURIComponent(modifiedHref);
-      },
+    buildTileUrlTemplate: null,
     stacProxyUrl: null,
     pathPrefix: "/",
     historyMode: "history",
     cardViewMode: "cards",
     cardViewSort: "asc",
-    showKeywordsInItemCards: true,
-    showKeywordsInCatalogCards: true,
-    showThumbnailsAsAssets: true,
+    showKeywordsInItemCards: false,
+    showKeywordsInCatalogCards: false,
+    showThumbnailsAsAssets: false,
     redirectLegacyUrls: false,
     itemsPerPage: 12,
-    defaultThumbnailSize: [300, 300],
-    maxPreviewsOnMap: 50,
+    maxItemsPerPage: 1000,
+    defaultThumbnailSize: null,
     crossOriginMedia: null,
     requestHeaders: {},
     requestQueryParameters: {},
-    // socialSharing: ['email', 'bsky', 'mastodon', 'x'],
-    socialSharing: [],
+    socialSharing: ['email', 'bsky', 'mastodon', 'x'],
     preprocessSTAC: null,
-<<<<<<< HEAD
     authConfig: null,
     crs: {}
-=======
-    authConfig: null
-    // authConfig:  {
-    //     type: "openIdConnect",  // (openIdConnect | apiKey | basicAuth | bearerToken | sessionKey).
-    //     description: "OIDC Auth Method",
-    //     name: "session_key",  // The name of the header, query, or cookie parameter to be used.
-    //     in: "query",  // (query | header | cookie).
-    //     cheme: "bearer",
-    //     formatter: token => `Bearer ${token}`,
-    //     low: "implicit", // (authorizationCode | implicit | password  | clientCredentials).
-    //     penIdConnectUrl: "http://localhost:8282/realms/aerofototeca/.well-known/openid-configuration",
-    //     idcConfig: {
-    //        client_id: "aerofototeca"
-    //     },
-    //     issuer: "http://localhost:8282/realms/aerofototeca",
-    // }
->>>>>>> 2a93bd82 (logo & colors)
 };
