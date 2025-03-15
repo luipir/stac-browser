25d2d7b32855015c50012d362346cc80510a482d
StefanoMorroni <smorroni@hotmail.com>
2025-03-15 17:31:49 +0100

    Matomo

--- src/StacBrowser.vue
+++ src/StacBrowser.vue
@@ -292,6 +292,12 @@ export default {
   mounted() {
     this.$root.$on('error', this.showError);
     setInterval(() => this.$store.dispatch('loadBackground', 3), 200);
+    $("button").attr("aria-label", "info");
+    $("a").attr("aria-label", "info");
+    $("img").attr("alt", "info");
+    $("a").attr("role", "link");
+    $("a").removeAttr("aria-selected");
+    $("a").removeAttr("aria-expanded");
   },
   methods: {
     ...mapActions(['switchLocale']),
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import init from "./init";
+import $ from 'jquery'
+window.$ = window.jQuery = $
 
 Vue.config.productionTip = false;
 
